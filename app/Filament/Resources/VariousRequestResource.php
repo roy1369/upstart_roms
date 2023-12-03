d2f242fdd3ad90593b03b1053fc90d77a51f27f7
roy1369 <ren.game.sh08@gmail.com>
2023-12-03 17:07:59 +0900

    リスト表示の作成と各種アクションの作成

--- app/Filament/Resources/VariousRequestResource.php
+++ app/Filament/Resources/VariousRequestResource.php
@@ -16,6 +16,10 @@ use Filament\Resources\Form;
 use Filament\Resources\Resource;
 use Filament\Resources\Table;
 use Filament\Tables;
+use Filament\Tables\Actions\Action;
+use Filament\Tables\Columns\Layout\Split;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\ViewColumn;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 
@@ -68,17 +72,126 @@ class VariousRequestResource extends Resource
     {
         return $table
             ->columns([
-                //
+                Split::make([
+                    ViewColumn::make('user_id')
+                        ->label('氏名')
+                        ->hidden((! auth()->user()->authority))
+                        ->toggleable(isToggledHiddenByDefault: false)
+                        ->view('tables.columns.user-name-switcher'),
+                    TextColumn::make('result')
+                        ->label('申請日')
+                        ->searchable()
+                        ->toggleable(isToggledHiddenByDefault: false)
+                        ->dateTime('Y年m月d日'),
+                    ViewColumn::make('type')
+                        ->label('申請種別')
+                        ->toggleable(isToggledHiddenByDefault: false)
+                        ->view('tables.columns.type-switcher'),
+                    TextColumn::make('correction_start_time')
+                        ->label('修正出勤時間')
+                        ->toggleable(isToggledHiddenByDefault: false)
+                        ->dateTime('G時i分'),
+                    TextColumn::make('correction_end_time')
+                        ->label('修正退勤時間')
+                        ->toggleable(isToggledHiddenByDefault: false)
+                        ->dateTime('G時i分'),
+                    TextColumn::make('correction_transportation_expenses')
+                        ->label('修正交通費')
+                        ->toggleable(isToggledHiddenByDefault: false)
+                        ->money('jpy'),
+                    TextColumn::make('comment')
+                        ->label('申請理由')
+                        ->toggleable(isToggledHiddenByDefault: false),
+                ])->from('md')
             ])
             ->filters([
                 //
             ])
             ->actions([
-                Tables\Actions\ViewAction::make(),
-                Tables\Actions\EditAction::make(),
+                Action::make('application')
+                    ->label('申請中')
+                    ->button()
+                    ->color('primary')
+                    ->visible(
+                        function ($record) :bool{
+                            $ret = false;
+                            // あとで結果に応じた表示の制御を追加
+                            $ret = true;
+                            return $ret;
+                        }
+                    ),
+                Action::make('application_end')
+                    ->label('承認済み')
+                    ->button()
+                    ->color('success')
+                    ->visible(
+                        function ($record) :bool{
+                            $ret = false;
+                            // あとで結果に応じた表示の制御を追加
+                            $ret = true;
+                            return $ret;
+                        }
+                    ),
+                Action::make('cancel_end')
+                    ->label('却下済み')
+                    ->button()
+                    ->color('danger')
+                    ->visible(
+                        function ($record) :bool{
+                            $ret = false;
+                            // あとで結果に応じた表示の制御を追加
+                            $ret = true;
+                            return $ret;
+                        }
+                    ),
+
+                // 管理者権限のみ表示するアクション
+                Action::make('approval')
+                    ->label('承認')
+                    ->button()
+                    ->color('secondary')
+                    ->requiresConfirmation()
+                    ->modalHeading('承認確認')
+                    ->modalsubheading('本当に承認しますか？')
+                    ->action(
+                        function ($record){
+                            // 承認時の処理を個々に記述
+
+                        }
+                    )
+                    ->visible(
+                        function ($record) :bool{
+                            $ret = false;
+                            // あとで承認か却下実施後は非表示になるように制御する　監理者のみ表示の制御も追加する
+                            $ret = true;
+                            return $ret;
+                        }
+                    ),
+                Action::make('cancel')
+                    ->label('却下')
+                    ->button()
+                    ->color('secondary')
+                    ->requiresConfirmation()
+                    ->modalHeading('却下確認')
+                    ->modalsubheading('本当に却下しますか？')
+                    ->action(
+                        function ($record){
+                            // 却下の処理を個々に記述
+
+                        }
+                    )
+                    ->visible(
+                        function ($record) :bool{
+                            $ret = false;
+                            // あとで承認か却下実施後は非表示になるように制御する　監理者のみ表示の制御も追加する
+                            $ret = true;
+                            return $ret;
+                        }
+                    ),
+                    
             ])
             ->bulkActions([
-                Tables\Actions\DeleteBulkAction::make(),
+                // 
             ]);
     }
     
@@ -94,8 +207,6 @@ class VariousRequestResource extends Resource
         return [
             'index' => Pages\ListVariousRequests::route('/'),
             'create' => Pages\CreateVariousRequest::route('/create'),
-            'view' => Pages\ViewVariousRequest::route('/{record}'),
-            'edit' => Pages\EditVariousRequest::route('/{record}/edit'),
         ];
     }    
 }
